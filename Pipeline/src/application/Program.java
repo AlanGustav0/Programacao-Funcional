68a6987e0d787930d07ff411008defed1bf2ed0f
AlanGustav0 <alangustavo946@gmail.com>
2021-08-23 09:28:37 -0300

    Ajuste de codigo pipeline

--- Pipeline/src/application/Program.java
+++ Pipeline/src/application/Program.java
@@ -12,7 +12,10 @@ public class Program {
 		
 		List<Integer> list = Arrays.asList(3,4,5,10,7);
 		
-		//Aqui transformamos a lista em uma nova stream de dados, porém mapeando os elementos e multiplicando todos por 10
+		/*Aqui transformamos a lista em uma nova stream de dados, porém mapeando os elementos e multiplicando todos por 10
+			A operação utilizada aqui foi a operação intermediária "map"
+		*/
+		
 		Stream<Integer> st1 = list.stream().map(x -> x * 10);
 		System.out.println(Arrays.toString(st1.toArray()));
 		
@@ -23,6 +26,7 @@ public class Program {
 		int sum = list.stream().reduce(0, (x,y) -> x + y);
 		System.out.println("Sum = " + sum);
 		
+		//Aqui um exemplo de operação intermediária "filter" e operação terminal "collect"
 		List<Integer> newList = list.stream()
 				.filter(x -> x % 2 == 0).map(x -> x * 10)
 				.collect(Collectors.toList());
